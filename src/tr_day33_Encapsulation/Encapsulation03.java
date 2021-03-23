f62310773945d3765d61c0fd88ca8ffda06fc5c1
ariomer <ariomer@gmail.com>
2021-03-23 20:29:58 +0000

    23 March_v2

--- src/tr_day33_Encapsulation/Encapsulation03.java
+++ src/tr_day33_Encapsulation/Encapsulation03.java
@@ -14,12 +14,16 @@ public class Encapsulation03 {
 		
 		System.out.println(obj1.sayi); // 100 public oldugun hem okudum
 		obj1.sayi=25;                  // hem de deger atayabildim
-		System.out.println(obj1.sayi);
-		System.out.println(obj1.getOkulIsmi()); // Yildiz Koleji
-		
+		System.out.println(obj1.sayi); //25
 		System.out.println(obj2.sayi); //100
 		
+		System.out.println(obj1.getOkulIsmi()); // Yildiz Koleji, get method'u oldugu icin sadece bilgi verir
+		
 		obj1.setHesapNo(546789);
+		obj1.setTcNo("3264578448");
+		//System.out.println(obj1.setTcNo("3264578448")); set() methodu okumaya izin vermez
+		
+		
 		// Bir Calss içinde class member oluşturuken bunu diğer class lar ile
 		// ne oranda paylaşacağımıza karar veririz
 		// 1- Herkes okuyabilsin ve obje üzerinden değer atayıp kullanbilsin ==> public
