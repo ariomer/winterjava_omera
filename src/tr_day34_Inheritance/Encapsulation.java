9075ace108cb4f55ff1ddbd1daf6c47822614537
ariomer <ariomer@gmail.com>
2021-03-24 20:20:14 +0000

    24 March

---
+++ src/tr_day34_Inheritance/Encapsulation.java
@@ -0,0 +1,32 @@
+package tr_day34_Inheritance;
+
+public class Encapsulation {
+	
+	private String okulIsmi = "Yildiz Koleji";
+	private int okulHesapNo = 12345;
+	private boolean okulAcikMi = true;
+
+	public String getOkulIsmi() {
+		return okulIsmi;
+	}
+
+	public int getOkulHesapNo() {
+		return okulHesapNo;
+	}
+	
+	public boolean isOkulAcikMmi() {
+		return okulAcikMi;
+	}
+	
+	public void setOkulIsmi(String okulIsmi) {
+		this.okulIsmi = okulIsmi;
+	}
+	
+	public void setokulHesapNo(int okulHesapNo) {
+		this.okulHesapNo = okulHesapNo;
+	}
+	
+	public void setokulAcikMi(boolean okulAcikMi) {
+		this.okulAcikMi = okulAcikMi;
+	}
+}
---
+++ src/tr_day34_Inheritance/EncapsulationRunner.java
@@ -0,0 +1,19 @@
+package tr_day34_Inheritance;
+
+public class EncapsulationRunner {
+
+	public static void main(String[] args) {
+		
+		Encapsulation obj1 = new Encapsulation();
+		System.out.println(obj1.getOkulIsmi());
+		System.out.println(obj1.getOkulHesapNo());
+		System.out.println(obj1.isOkulAcikMmi());
+		
+		obj1.setOkulIsmi("Mehmet Koleji");
+		System.out.println(obj1.getOkulIsmi()); // Mehmet Koleji
+		Encapsulation obj2 = new Encapsulation();
+		System.out.println(obj2.getOkulIsmi()); // Yildiz Koleji
+
+	}
+
+}
