26739e73de7cc7df15804d51ab88ad830a69d7a9
ariomer <ariomer@gmail.com>
2021-03-30 21:25:42 +0100

    30 March

---
+++ src/tr_day39_exceptions/Exceptions01.java
@@ -0,0 +1,26 @@
+package tr_day39_exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Exceptions01 {
+
+	public static void main(String[] args) {
+		
+		// Java'ya bir dosyayi okumasini veya dosyaya yazmasini soyledigimizde
+		// Java "Ya dosyayi bulamazsam" der ve bizden cozum uretmemizi bekler
+		// dosya bulma ile ilgili exception turu : FileNotFoundException
+		// Java kodu yazar yazmaz buradaki olasi problemi gorur ve CTE verir
+		// Kodumuzu yazdigimiz anda ortaya cikan bu tur exception'lara Checked Exceptions diyoruz
+		//
+		
+		try {
+			FileInputStream fis = new FileInputStream("C:\\Users\\ariom\\eclipse-workspace\\winterjava_omera\\src\\tr_day39_exceptions\\File");
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace(); // tum hata aciklamalarini yazdirir ama kodumuz bloke olmaz
+		}
+		System.out.println("Kod bloke olmamis...");
+	}
+
+}
