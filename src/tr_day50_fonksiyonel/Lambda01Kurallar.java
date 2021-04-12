665e5ceb0ed23d94b1c3169abc05275bd0fd8ed6
ariomer <ariomer@gmail.com>
2021-04-12 17:27:31 +0100

    12 April

---
+++ src/tr_day50_fonksiyonel/Lambda01Kurallar.java
@@ -0,0 +1,14 @@
+package tr_day50_fonksiyonel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda01Kurallar {
+
+	public static void main(String[] args) {
+		List<Integer> liste = Arrays.asList(1,2,3,4,5);
+        liste.forEach(x -> System.out.println(x));
+
+	}
+
+}
